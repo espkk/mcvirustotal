5939a9d160ccf33ccdbd4dd1ab4b992437bb601a
espkk <espkk@protonmail.ch>
2017-12-02 17:58:57 +0300

    CopyData, request_ptr

--- MCVirusTotal/VirusTotal.cpp
+++ MCVirusTotal/VirusTotal.cpp
@@ -1,6 +1,7 @@
 #include "VirusTotal.h"
 #include "json.hpp"
 #include <IHTTPRequest.h>
+#include <memory>
 
 using namespace MCNS;
 
@@ -9,63 +10,57 @@ void VirusTotal::SetApiKey(const char *ApiKey)
 	strcpy(m_ApiKey, ApiKey);
 }
 
+class request_ptr {
+public:
+	request_ptr(IHTTPRequest *pRequest) : m_pRequest(pRequest) { }
+	~request_ptr() { m_pRequest->Release(); }
+	IHTTPRequest* operator->() { return m_pRequest; }
+	IHTTPRequest& operator* () { return *m_pRequest; }
+private:
+	IHTTPRequest *m_pRequest;
+};
+
 std::wstring VirusTotal::CheckByHash(const char *hash)
 {
 	using namespace MCNS;
 	using namespace nlohmann;
 
 	std::string request = FormatRequest("file/report", hash);
-	IHTTPRequest* pRequest = (*m_ppAppInterface)->CreateHTTPRequester();
+
+	request_ptr pRequest = (*m_ppAppInterface)->CreateHTTPRequester();
 	IHTTPResponse* pResponse = pRequest->SendRequestGET(request.c_str());
 
-	BYTE *data;
 	int ret = pRequest->GetResponseCode();
 	try
 	{
 		if (ret == 403) // api key not present/incorrect/banned/not enough permissions
-			throw L"Invalid API key";
+			return L"Invalid API key";
+
 		if (ret == 204) // 4 request/sec exceeded
-			throw L"Limit exceeded";
+			return L"Limit exceeded";
 
 		if (ret != 200 || pResponse == nullptr)
-			throw L"Unknown error";
+			return L"Unknown error";
 
 		size_t data_len = pResponse->DataLen();
-		data = new BYTE[data_len + 1];
+		std::unique_ptr<BYTE[]> data(new BYTE[data_len + 1]);
 		if (data == nullptr)
-			throw L"Unknown error";
+			return L"Unknown error";
+		pResponse->CopyData(data.get(), data_len + 1);
 
-		strncpy((char*)data, (const char*)pResponse->Data(), data_len);
-		data[data_len] = '\0';
-
-		json response = json::parse(data);
-
-		free(data);
-		data = nullptr;
+		json response = json::parse(data.get());
 
 		int response_code = response["response_code"];
 		if (response_code != 1) // 0 = not found; -2 = queued
-			throw L"N/A";
+			return L"N/A";
 
 		int positives = response["positives"];
 		int total = response["total"];
 
 		return std::to_wstring(positives) + L"/" + std::to_wstring(total);
 	}
-	catch(const WCHAR *s)
+	catch(...) // json error — detail::type_error
 	{
-		pRequest->Release();
-		if(data != nullptr)
-			free(data);
-
-		return s;
-	}
-	catch(...) //detail::type_error e
-	{
-		pRequest->Release();
-		if (data != nullptr)
-			free(data);
-
 		return L"Unknown error";
 	}
 }
